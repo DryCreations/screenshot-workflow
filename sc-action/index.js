dd77e68655ca0f515d8610b256fefaa309da5697
DryCreations <DryCreations@gmail.com>
2021-11-30 17:43:14 -0500

    upload images

--- sc-action/index.js
+++ sc-action/index.js
@@ -21,7 +21,7 @@ try {
     const browser = await webkit.launch();
     const page = await browser.newPage();
     await page.goto('http://whatsmyuseragent.org/');
-    await page.screenshot({ path: `example.png` });
+    await page.screenshot({ path: `image-uploads/example.png` });
     await browser.close();
   })();  
 
